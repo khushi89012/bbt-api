72684394124341be5e79a9c26a8361d6083c698e
khushiindusnet <khushi.gupta@indusnet.co.in>
2024-12-31 17:22:04 +0530

    Backend modified

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ const app = express();
 // Static folder to serve uploaded files (optional)
 app.use('/uploads', express.static('uploads'));
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    credentials: true, // If you need cookies or authorization headers
-  }));
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
